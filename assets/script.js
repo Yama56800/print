2fe796eb66066e9c9b44fcbe0c0a692502bfbfb7
Yama56800 <thomawmahiaw@gmail.com>
2023-08-23 21:03:58 +0200

    good commit

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,7 @@ const slides = [
 
 // objets
 const items = document.querySelectorAll('.banner-img img')
-const nbSlide = items.length; 
+const nbSlide = slides.length; 
 const suivant = document.querySelector('#r')
 const precedent = document.querySelector('#l')
 
@@ -89,13 +89,16 @@ document.addEventListener('DOMContentLoaded', function() {
 precedent.addEventListener('click', function(){  
 	if(count < slides.length){
 		dot[count].classList.remove('dot_selected');
-		count--;
+		if (count == 0){
+			count = slides.length -1;
+		}
+		else{
+			count --;
+		}
 		dotmove();
 		text.innerHTML = slides[count].tagLine;
 		img.src = "./assets/images/slideshow/" + slides[count].image; 
-	} else{
-		count = slides.length ;
-	}
+	} 
 	
 	console.log("valeur initial",img.src)
 
@@ -109,7 +112,7 @@ function dotmove(){
 	
 	
 	if(count < slides.length){
-		
+		console.log(count,dot)
 		dot[count].classList.add('dot_selected')
 		
 		
@@ -128,41 +131,41 @@ suivant.addEventListener('click', dotmove)
 // code qui permet que le dot sois coché au clic
 point1 = document.querySelector('.point1')
 point1.addEventListener('click', function(){
-	
-	img.src = "./assets/images/slideshow/" + slides[count = 0].image
-dot[count].classList.add('dot_selected')
-text.innerHTML = slides[count].tagLine
+	dot[count].classList.remove('dot_selected');
+	img.src = "./assets/images/slideshow/" + slides[count = 0].image;
+	dot[count].classList.add('dot_selected');
+	text.innerHTML = slides[count].tagLine;
 })
 
 point2 = document.querySelector('.point2')
 point2.addEventListener('click', function(){
-	
-	img.src = "./assets/images/slideshow/" + slides[count = 1].image
-dot[count].classList.add('dot_selected')
-text.innerHTML = slides[count].tagLine
+	dot[count].classList.remove('dot_selected');
+	img.src = "./assets/images/slideshow/" + slides[count = 1].image;
+	dot[count].classList.add('dot_selected');
+	text.innerHTML = slides[count].tagLine;
 })
 
 point3 = document.querySelector('.point3')
 point3.addEventListener('click', function(){
-
-	img.src = "./assets/images/slideshow/" + slides[count = 2].image
-	dot[count].classList.add('dot_selected')
-	text.innerHTML = slides[count].tagLine
+	dot[count].classList.remove('dot_selected');
+	img.src = "./assets/images/slideshow/" + slides[count = 2].image;
+	dot[count].classList.add('dot_selected');
+	text.innerHTML = slides[count].tagLine;
 })
 
 point4 = document.querySelector('.point4')
 point4.addEventListener('click', function(){
-
-	img.src = "./assets/images/slideshow/" + slides[count = 3].image
-	dot[count].classList.add('dot_selected')
-	text.innerHTML = slides[count].tagLine
+	dot[count].classList.remove('dot_selected');
+	img.src = "./assets/images/slideshow/" + slides[count = 3].image;
+	dot[count].classList.add('dot_selected');
+	text.innerHTML = slides[count].tagLine;
 })
 
 
 //code pour que ca reset les dot avec la fleche suivante
 document.addEventListener('click', function() {
     // Code à exécuter lorsque la page est chargée
-    dot[count.length].classList.remove('dot_selected');
+   
 });
 
 
